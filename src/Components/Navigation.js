71eddef52ea89df78e9980704e719ea061b477d4
unsoul7 <disketteit@gmail.com>
2022-08-14 01:08:23 +0530

    Adding new files and Navbar

---
+++ src/Components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from "./logo.png"
+
+function Navigation() {
+  return (
+    <div className='navbar'>
+        <img src={logo} alt="website-logo" />
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Categories</a></li>
+            <li><a href="#">Info</a></li>
+        </ul>
+        <div>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation   
